ba6bd3a1ee29e4c63be05c8167c334d4f2376621
DavideSias <davidesias77@gmail.com>
2022-11-09 18:40:53 +0100

    Add random color to icons

--- js/script.js
+++ js/script.js
@@ -113,20 +113,64 @@ const arrIcons = [
 	}
 ];
 
+function getRandomColor(){
+    let color = Math.floor(Math.random()*16777215).toString(16);
+    return color
+}
+
+const arrRandomColors = [];
+
 const eleContainer = document.querySelector('.container');
 const eleSelect = document.querySelector('#icons');
 
 for(let i = 0; i < arrIcons.length; i++){
+
+    arrRandomColors.push(getRandomColor());
+    console.log(arrRandomColors);
+
     const eleCard = document.createElement('div');
     eleCard.classList.add('card');
     
     eleCard.innerHTML = `
-    <i class="${arrIcons[i].family} ${arrIcons[i].prefix}${arrIcons[i].name} ${arrIcons[i].type}" style="color: ${arrIcons[i].color}"></i>
+    <i class="${arrIcons[i].family} ${arrIcons[i].prefix}${arrIcons[i].name} ${arrIcons[i].type}" style="color: #${arrRandomColors[i]}"></i>
     <div class="text">${arrIcons[i].name}</div>
     `
 
     eleContainer.append(eleCard);
 }
 
+// let listTypeAnimal = document.querySelectorAll('.animal');
+// let listTypeVegetable = document.querySelectorAll('.vegetable');
+// let listTypeUser = document.querySelectorAll('.user');
+// let listTypeAll = document.querySelectorAll('i');
 
+// // console.log(listTypeAnimal);
+// // console.log(listTypeVegetable);
+// // console.log(listTypeUser);
+// // console.log(listTypeAll);
+
+// for(let i = 0; i < listTypeAll.length; i++){
+//     if(eleSelect.value == 'all'){
+//         listTypeAll[i].classList.add('visible');
+//     } else if(eleSelect.value == 'animal'){
+//         listTypeAnimal[i].classList.add('visible');
+//     } else if(eleSelect.value == 'vegetable'){
+//         listTypeVegetable[i].classList.add('visible');
+//     } else {
+//         listTypeUser[i].classList.add('visible');
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+  
 
